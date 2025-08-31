7ce4e225521cbcdadca1df1ab64fa6cc850b13ce
Tarun <anikguptatarun@gmail.com>
2025-08-31 20:03:13 +0600

    modifed for deployment

--- server/db/index.js
+++ server/db/index.js
@@ -1,19 +1,18 @@
 // db.js
-const { Pool } = require('pg'); // Use CommonJS require
+const { Pool } = require('pg');
 
-// Database Configuration using environment variables
+// Database Configuration using a single DATABASE_URL environment variable
 const pool = new Pool({
-    user: process.env.PGUSER,
-    host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 // Add an error listener to the pool to catch connection errors
 pool.on('error', (err) => {
-  console.error('Unexpected error on idle client', err);
-  process.exit(-1); // Exit the process if a database error occurs
+    console.error('Unexpected error on idle client', err);
+    process.exit(-1); // Exit the process if a database error occurs
 });
 
 // Export the query function and the pool instance using CommonJS module.exports
